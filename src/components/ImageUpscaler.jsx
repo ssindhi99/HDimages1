58802c214196cea599ace7fc47dfcafbf0260aa8
ssindhi99 <sonusindhi099@gmail.com>
2025-05-20 18:56:41 +0530

    Update ImageUpscaler.jsx

--- src/components/ImageUpscaler.jsx
+++ src/components/ImageUpscaler.jsx
@@ -14,8 +14,8 @@ const ImageUpscaler = () => {
 
       return new Promise((resolve) => {
         img.onload = () => {
-          const minWidth = 2048;
-          const minHeight = 1536;
+          const minWidth = 1610;
+          const minHeight = 1210;
           const scaleWidth = minWidth / img.width;
           const scaleHeight = minHeight / img.height;
           const scaleFactor = Math.max(scaleWidth, scaleHeight, 1);
